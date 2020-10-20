9be4e6089bad7eecf83ba36e234a4b07bde068d5
Rachael <rachael@bodigiti.com>
2020-10-20 17:35:11 -0400

    Added reduce function to Burger.js to flatten the mapped array so that we can access the values in the inner array

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -5,12 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys( props.ingredients )
+  let transformedIngredients = Object.keys( props.ingredients )
     .map(ingKey => {
       return [...Array( props.ingredients[ingKey] )].map( (_, i) => {
         return <BurgerIngredient key={ingKey + i} type={ingKey} />;
       } );
-    } );
+    } )
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please add your ingredients!</p>
+    }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
