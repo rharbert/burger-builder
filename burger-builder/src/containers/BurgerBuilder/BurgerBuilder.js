f1d78ec0c5b7013f3b3a3bbb93ed0708c8a25ca8
Rachael <rachael@bodigiti.com>
2020-10-22 10:19:38 -0400

    Connnected Less button to removeHandler and added a disabled functionality (which I feel is unnecessary... at least at this point)

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,15 +36,38 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
-
+    const oldCount = this.state.ingredients[type];
+    //if Statement assures that nothing happens if we try to reduce (clicking the Less button) for an ingredient which we didn't add
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
   
   render () {
+    /* I feel that this disabledInfo code and the disabled property in the BuildControls below is unnecessary */ 
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    } /* End unnecessary */
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
-          ingredientAdded={this.addIngredientHandler} />
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabledInfo={disabledInfo} /* unnecessary*/ />
       </Aux>
     );
   }
