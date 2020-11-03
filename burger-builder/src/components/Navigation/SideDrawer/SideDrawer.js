1f9a78d6d39c2c93688a83570cee96140e37e310
Rachael <rachael@bodigiti.com>
2020-11-03 14:06:41 -0500

    Added SideDrawer toggle button

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -16,7 +16,7 @@ const sideDrawer = (props) => {
   return (
     <Aux>
       <BackDrop show={props.open} clicked={props.closed} />
-      <div className={attachedClasses.join(' ')} >
+      <div className={attachedClasses.join(' ')}>
         <Logo height="12%"/>
         <nav>
           <NavItems />
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,12 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <Logo height="80%"/>
     <nav className={classes.DesktopOnly}>
       <NavItems />
