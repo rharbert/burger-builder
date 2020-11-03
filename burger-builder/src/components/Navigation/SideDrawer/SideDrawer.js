5aa849e80c01cba02d4e8aceba0f948db6a9bd0c
Rachael <rachael@bodigiti.com>
2020-11-03 13:52:13 -0500

    Reused Backdrop and adjusted Layout into a class based Component to determine when the SideDrawer shows and when it does not; in other words, determine when SideDrawer is open or closed

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,16 +4,25 @@ import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 
 import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
-  //..
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses =[classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="12%"/>
-      <nav>
-        <NavItems />
-      </nav>
-    </div>
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')} >
+        <Logo height="12%"/>
+        <nav>
+          <NavItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
