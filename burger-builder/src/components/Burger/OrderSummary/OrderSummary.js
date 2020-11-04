6f118a55b0c9091cb09d25893eb904ae35955259
Rachael <rachael@bodigiti.com>
2020-11-04 09:13:25 -0500

    Converted OrderSummary.js into class based component so that we can apply some lifecycle hooks to prevent unnecessary re-rendering of it in the Modal located in BurgerBuilder.js when Modal is not visible. Will apply hooks in next commit.

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(ingKey => {
       return (
         <li key={ingKey}>
-          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
         </li>);
     } );
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Your delicious burger is topped with the following...</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button
-        btnType="Danger"
-        clicked={props.purchaseCanceled}
-      >CANCEL</Button>
-      <Button
-        btnType="Success"
-        clicked={props.purchaseContinue}
-      >CONTINUE</Button>
-    </Aux>
-  );
-};
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Your delicious burger is topped with the following...</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button
+          btnType="Danger"
+          clicked={this.props.purchaseCanceled}
+        >CANCEL</Button>
+        <Button
+          btnType="Success"
+          clicked={this.props.purchaseContinue}
+        >CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
