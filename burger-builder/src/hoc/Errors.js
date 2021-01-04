afe2b80dc61ca46d5fd926e25568e4d3d0898a0e
Rachael <rachael@bodigiti.com>
2021-01-04 17:43:09 -0500

    Added componentWillUnmount to Clean Up (aka remove intercepts when Error Handler is used on other components other than BurgerBuilder)

--- burger-builder/src/hoc/Errors.js
+++ burger-builder/src/hoc/Errors.js
@@ -9,17 +9,23 @@ const errors = (WrappedComponent, axios ) => {
       error: null
     }
     componentWillMount () { //WillMount will soon be unsupported; the Work Around = use constructor
-      axios.interceptors.request.use(req => {
-        this.setState({error: null});
+      this.requestInterceptor = axios.interceptors.request.use(req => {
+        this.setState( {error: null} );
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
-        this.setState({error: error});
+      this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
+        this.setState( {error: error} );
       });
     }
 
+    // Cleanup work: stop calling the interceptors when Error Handler is used on other pages other than Burger Builder
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
+    } // End Cleanup work
+
     errorConfirmedHandler = () => {
-      this.setState({error:null})
+      this.setState( {error:null} )
     }
     
     render () {
